444363b90b8010b3d41ce90b7a2077249e8a4523
Avill2 <andrea.villacis@epn.edu.ec>
2018-07-01 22:34:05 -0500

    arreglado el speech to text

--- app/src/main/java/com/example/andreavillacis/speechtotext/MainActivity.kt
+++ app/src/main/java/com/example/andreavillacis/speechtotext/MainActivity.kt
@@ -1,50 +1,50 @@
 package com.example.andreavillacis.speechtotext
 
-import android.os.Build
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.speech.tts.TextToSpeech
-import android.support.annotation.RequiresApi
-import android.util.Log
-import kotlinx.android.synthetic.main.activity_main.*
-import org.w3c.dom.Text
-import java.util.*
-import kotlin.math.log
 
-class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
-    var tts:TextToSpeech?=null
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txtResultado.findViewById(R.id.txtResultado) as TextView
 
-        button_speak.isEnabled=false
-        button_speak.setOnClickListener{speakOut()}
-        tts=  TextToSpeech(this,this)
     }
 
-    override fun onInit(status: Int) {
-        if(status == TextToSpeech.SUCCESS){
-            val result = tts!!.setLanguage(Locale.US)
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
-                Log.e("TTS", "El idioma especificado no es compatible")
-            }else{
-                button_speak.isEnabled = true
+    fun getSpeechInput (view:View){
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault())
+        if (intent.resolveActivity(getPackageManager()) !=null){
+            startActivityForResult(intent,10)
+        }
+        else{
+            Toast.makeText(this,"\n" +
+                    "Su dispositivo no es compatible con la entrada de voz",Toast.LENGTH_SHORT ).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode:Int, resultCode:Int, data:Intent){
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode){
+            10 -> if (resultCode == Activity.RESULT_OK && data !=null){
+                val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                txtResultado.setText(result.get(0))
+
             }
-        }else
-        {
-            Log.e("TTS", "La iniciacion falló")
         }
-    }
-    private fun speakOut() {
-       val texto = txt_input.toString()
-        tts!!.speak(texto, TextToSpeech.QUEUE_FLUSH, null,"")
-    }
-    override fun onDestroy() {
-        if (tts != null){
-            tts!!.stop()
-            tts!!.shutdown()
-        }
-        super.onDestroy()
     }
 }
